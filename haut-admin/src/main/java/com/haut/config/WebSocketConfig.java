77d0517e6e095e385e0c311b797e7bb8f705a628
Chenggou <Chenggou@qq.com>
2023-12-11 17:10:53 +0800

    实现了 /tumble/processImg 接口 ， 该接口接受一个post请求， 内容是 png图片的base64编码， 返回一张jpg图片的base64编码。

---
+++ haut-admin/src/main/java/com/haut/config/WebSocketConfig.java
@@ -0,0 +1,13 @@
+package com.haut.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
---
+++ haut-common/src/main/java/com/haut/common/DataEntity/ImageData.java
@@ -0,0 +1,14 @@
+package com.haut.common.DataEntity;
+
+
+public class ImageData {
+    private String imageData;
+
+    public String getImageData() {
+        return imageData;
+    }
+
+    public void setImageData(String imageData) {
+        this.imageData = imageData;
+    }
+}
